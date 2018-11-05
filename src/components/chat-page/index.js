f90ec177356011d172606d7c52a681e5ba4718cc
Entony <tony@MacBook-Air-Entony.local>
2018-11-06 00:53:23 +0200

    fix: fix chat block

--- src/components/chat-page/index.js
+++ src/components/chat-page/index.js
@@ -1,55 +1,63 @@
 import React, { Component } from 'react';
-
-const chat = [
-  { id: 1, text: 'ololo' },
-  { id: 2, text: 'ololoaad' },
-  { id: 3, text: 'ololoccv' },
-  { id: 4, text: 'ololo333' },
-  { id: 5, text: 'olfdsolo' },
-  { id: 6, text: 'ololo231' },
-];
+import PropTypes from 'prop-types';
+import moment from 'moment';
 
 export default class ChatPage extends Component {
-  state = {
-    text: '',
-  };
+	static propTypes = {
+		messages: PropTypes.array,
+		user: PropTypes.object,
+	};
 
-  handleChange = e => this.setState({ text: e.target.value });
+	state = {
+		text: '',
+	};
 
-  handleSubmit = () => {
-    const { text } = this.state;
-    const { user, createMessage } = this.props;
+	handleChange = e => this.setState({ text: e.target.value });
 
-    createMessage({ message: text, uid: user.uid });
-  };
+	handleSubmit = () => {
+		const { text } = this.state;
+		const { user, createMessage } = this.props;
 
-  render() {
-    const { text } = this.state;
-    return (
-      <div className="chat-block">
-        <div className="chat-block__message-area">
-          {chat.map(el => (
-            <div className="chat-block__message" key={el.id}>
-              {el.text}
-            </div>
-          ))}
-        </div>
-        <div className="chat-block__input-area">
-          <input
-            onChange={this.handleChange}
-            placeholder="Say something"
-            type="text"
-            className="chat-block__input"
-          />
-          <button
-            onClick={this.handleSubmit}
-            disabled={text === ''}
-            className="button button--white"
-          >
-            submit
-          </button>
-        </div>
-      </div>
-    );
-  }
+		createMessage({ message: text, uid: user.uid });
+	};
+
+	render() {
+		const { text } = this.state;
+		const { messages, user } = this.props;
+		console.log('dsf', this.props);
+		return (
+			<div className="chat-block">
+				<div className="chat-block__message-area">
+					{messages.filter(el => el.content).map(el => (
+						<div
+							key={el.messageId}
+							className={
+								el.uid === user.uid ? 'chat-block__message chat-block__message--self' : 'chat-block__message'
+							}>
+							<div
+								className={
+									el.uid !== user.uid
+										? 'chat-block__message-time chat-block__message-time--self'
+										: 'chat-block__message-time'
+								}>
+								{moment(el.timeStamp).format('HH:mm')}
+							</div>
+							{el.content}
+						</div>
+					))}
+				</div>
+				<div className="chat-block__input-area">
+					<input
+						onChange={this.handleChange}
+						placeholder="Say something"
+						type="text"
+						className="chat-block__input"
+					/>
+					<button onClick={this.handleSubmit} disabled={text === ''} className="button button--white">
+						submit
+					</button>
+				</div>
+			</div>
+		);
+	}
 }
--- src/reducers/messages/index.js
+++ src/reducers/messages/index.js
@@ -2,17 +2,16 @@ import * as types from '../../constants/actionTypes';
 import initialState from '../initial-state';
 
 export default (state = initialState.messages, { type, payload }) => {
-  switch (type) {
-    case types.ADD_MESSAGE: {
-      console.log('ADD_MESSAGE', payload);
-      const newMessage = { ...payload.val, messageId: payload.key };
-      return { ...state, data: state.data.concat(newMessage) };
-    }
-    case types.REMOVE_MESSAGE: {
-      console.log('REMOVE_MESSAGE', payload);
-      return state;
-    }
-    default:
-      return state;
-  }
+	switch (type) {
+		case types.ADD_MESSAGE: {
+			const newMessage = { ...payload.val, messageId: payload.key };
+			return { ...state, data: state.data.concat(newMessage) };
+		}
+		case types.REMOVE_MESSAGE: {
+			console.log('REMOVE_MESSAGE', payload);
+			return state;
+		}
+		default:
+			return state;
+	}
 };
